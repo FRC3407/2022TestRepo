af921a2c347807322617b21d8dedd2d74a0a4dfd
Zoke Sackih <zokesackih@gmail.com>
2022-12-13 20:28:18 -0600

    added more base methods

--- src/main/java/frc/robot/CLDriveBase.java
+++ src/main/java/frc/robot/CLDriveBase.java
@@ -10,12 +10,15 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
 import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
 import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.constraint.DifferentialDriveVoltageConstraint;
 import frc.robot.ADIS16470;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.geometry.Rotation2d;
 
-public class CLDriveBase extends SubsystemBase {
+public class CLDriveBase extends SubsystemBase { //CL Stands for Closed Loop
 
-    private final WPI_TalonSRX 
+    private final WPI_TalonSRX
         leftf = new WPI_TalonSRX(0),
         leftb = new WPI_TalonSRX(1),
         rightf = new WPI_TalonSRX(3),
@@ -32,7 +35,7 @@ public class CLDriveBase extends SubsystemBase {
     private final DifferentialDriveOdometry odometry;
     private final DifferentialDriveKinematics kinematics = new DifferentialDriveKinematics(Constants.trackwidth);;
 
-    public CLDDriveBase(){
+    public CLDriveBase(){
         this.odometry = new DifferentialDriveOdometry(gyro.getRotation2d()); // gets angle so it knows where to start from
 
         leftf.configFactoryDefault(); // sets all to factory default value
@@ -54,19 +57,124 @@ public class CLDriveBase extends SubsystemBase {
         leftb.setInverted(InvertType.FollowMaster); // inverts these too
         rightb.setInverted(InvertType.FollowMaster);
 
-        
+    
+    }
+
+    public Pose2d getCurrtentPose() {  // in meters // gets where it is on a 2d plane and direction, uses odometry to help
+    return this.odometry.getPoseMeters();
+    }
+
+    public DifferentialDriveWheelSpeeds getWheelSpeeds(){
+        return new DifferentialDriveWheelSpeeds(this.getLeftVelocity(), this.getRightVelocity());
+    }
+
+    public double getRawLeftPosition() {
+		return this.leftf.getSelectedSensorPosition();
+	}
+	public double getRawRightPosition() {
+		return this.rightf.getSelectedSensorPosition();
+	}
+	public double getRawLeftVelocity() {
+		return this.leftf.getSelectedSensorVelocity();
+	}
+	public double getRawRightVelocity() {
+		return this.rightf.getSelectedSensorVelocity();
+	}
+    
+    public double getLeftPositionMeters() { // convert number of ticks to meters
+        return this.getRawLeftPosition()
+            / Constants.srx_mag_units_per_revolution
+            * Constants.drivewheel_diameter_meters * Math.PI;
+		
+	}
+    public double getRightPositionMeters() { // convert number of ticks to meters
+        return this.getRawRightPosition()
+            / Constants.srx_mag_units_per_revolution
+            * Constants.drivewheel_diameter_meters * Math.PI;
+		
+	}
+
+    public double getLeftVelocity() {
+		return this.getRawLeftVelocity()
+            * 10
+            / Constants.srx_mag_units_per_revolution
+            * Constants.drivewheel_diameter_meters * Math.PI;
+	}
+	public double getRightVelocity() {
+		return this.getRawRightVelocity()
+            * 10
+            / Constants.srx_mag_units_per_revolution
+            * Constants.drivewheel_diameter_meters * Math.PI;
+	}
+	
+    public double getContinuousAngle() { // in degrees
+        return this.gyro.getAngle();
+    }
+
+    public double getHeading() { // from 180 to -180 // helps to figure out if youre facing left or right
+        return this.gyro.getRotation2d().getDegrees();
+    }
+
+    public double getTurnRate() { // in degrees per second
+        return -this.gyro.getRate(); // positive is counterclockwise 
+    }
+
+    public Rotation2d getRotation() {
+        return this.gyro.getRotation2d();
+    }
+
+    public DifferentialDriveVoltageConstraint getVoltageConstraint(){
+        return new DifferentialDriveVoltageConstraint(
+            this.feedforward, this.kinematics, Constants.drivebase_max_voltage
+            );
+    }
+    public TrajectoryConfig getTrajectoryConfig() {
+        return new TrajectoryConfig(
+            Constants.max_velocity_meters_per_sec,
+            Constants.max_acceleration_meters_per_sec_sqrd
+        ).setKinematics(
+            this.kinematics
+        ).addConstraint(
+            this.getVoltageConstraint()
+        );
+    }
+
+    // // "Setters" -> require command key
+	// public void resetOdometry(Pose2d p, CLDriveCommand c) {
+	// 	this.resetEncoders(c);
+	// 	this.position_offset = this.getTotalPose();
+	// 	this.odometry.resetPosition(p, this.getRotation());
+	// }
+	// public void setDriveVoltage(double lv, double rv, CLDriveCommand c) {
+	// 	this.left.setVoltage(lv);
+	// 	this.right.setVoltage(rv);
+	// 	super.getDrive().feed();
+	// }
+	// public void resetEncoders(CLDriveCommand c) {
+	// 	this.left.setSelectedSensorPosition(0.0);
+	// 	this.right.setSelectedSensorPosition(0.0);
+	// }
+	// public void zeroHeading(CLDriveCommand c) {
+	// 	this.gyro.reset();
+	// }
+
+	// public void setInitial(Pose2d init) {	// set the initial position based on where the robot is located on the field - used primarily for accurate dashboard view
+	// 	if(this.position_offset.equals(new Pose2d())) {
+	// 		this.position_offset = init;
+	// 	}
+	// }
 
 
 
+    
+    public static class FollowTrajectory extends CLDriveCommand {
 
-
+        private final Trajectory trajectory;
 
 
 
     }
 
 
-
- 
 
 }
--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -3,8 +3,8 @@ package frc.robot;
 import edu.wpi.first.math.util.Units;
 
 public final class Constants {
-    public static final double kMaxSpeedMetersPerSecond = 1.5;
-    public static final double kMaxAccelerationMetersPerSecondSquared = 1.5;
+    public static final double max_velocity_meters_per_sec = 1.5;
+    public static final double max_acceleration_meters_per_sec_sqrd = 1.5;
 
     // Reasonable baseline values for a RAMSETE follower in units of meters and seconds
     public static final double kRamseteB = 2;
@@ -16,6 +16,11 @@ public final class Constants {
     public static final double drivebase_kP = 3.5176;	// "kP"(volts * seconds / meters) -> voltage added to correct for error
 
     public static final double trackwidth = Units.inchesToMeters(21.819);
+    public static final double srx_mag_units_per_revolution = 4096.0;
+
+    public static final double drivewheel_diameter_meters = Units.inchesToMeters(6.0);
+
+    public static final double drivebase_max_voltage = 10.0; 
 
 
     
